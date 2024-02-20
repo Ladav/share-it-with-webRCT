d5ef3d00119786bf53271eaf4e4f0a9e24999c5b
Ladav <davindersaini003@gmail.com>
2024-02-21 00:07:32 +0530

    feat: setup text based chat í´¥

--- src/pages/initiator/initiator.tsx
+++ src/pages/initiator/initiator.tsx
@@ -5,6 +5,8 @@ export default function Initiator() {
   const [peer, setPeer] = useState<SimplePeer.Instance>()
   const [signals, setSignals] = useState<object[]>([])
   const [answer, setAnswer] = useState("")
+  const [messages, setMessages] = useState<string[]>([])
+  const [composedMessage, setComposedMessage] = useState("")
 
   useEffect(() => {
     if (!peer) {
@@ -17,43 +19,82 @@ export default function Initiator() {
 
       newPeer.on("connect", () => {
         console.log("## CONNECTED")
-        newPeer.send("whatever" + Math.random())
+        newPeer.send("Connected")
+      })
+
+      newPeer.on("data", (data: Uint8Array) => {
+        // Convert the Uint8Array to a Buffer
+        const buffer = Buffer.from(data)
+        // Convert the Buffer to text using toString() method
+        const text = buffer.toString("utf-8")
+        setMessages((prev) => [...prev, text])
       })
 
       setPeer(newPeer)
     }
   }, [signals, peer])
 
-  console.log(peer)
+  console.log({ peer, connected: peer?.connected })
 
   return (
     <div>
       <h1>Initiator Page</h1>
-      <div>
-        <input
-          type="text"
-          name="answer"
-          id="answer"
-          onChange={(e) => {
-            setAnswer(e.target.value)
-          }}
-        />
-        <button
-          onClick={() => {
-            peer?.signal(answer)
-          }}
-        >
-          Send Answer
-        </button>
-      </div>
-      <div>
-        <h2>Signals</h2>
-        <ul>
-          {signals.map((item) => (
-            <li key={JSON.stringify(item)}>{JSON.stringify(item, null, 2)}</li>
-          ))}
-        </ul>
-      </div>
+      {peer?.connected ? (
+        <div>
+          <h2>Messsages</h2>
+          <ul>
+            {messages.map((msg) => (
+              <li key={msg}>{msg}</li>
+            ))}
+          </ul>
+          <div>
+            <input
+              type="text"
+              name="message"
+              id="message"
+              onChange={(e) => {
+                setComposedMessage(e.target.value)
+              }}
+            />
+            <button
+              onClick={() => {
+                peer.send(composedMessage)
+                setComposedMessage("")
+              }}
+            >
+              Send Answer
+            </button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div>
+            <input
+              type="text"
+              name="answer"
+              id="answer"
+              onChange={(e) => {
+                setAnswer(e.target.value)
+              }}
+            />
+            <button
+              onClick={() => {
+                peer?.signal(answer)
+              }}
+            >
+              Send Answer
+            </button>
+          </div>
+          <div>
+            <h2>Signals</h2>
+            <ul>
+              {signals.map((item) => (
+                <li key={JSON.stringify(item)}>{JSON.stringify(item, null, 2)}</li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/pages/receiver/receiver.tsx
+++ src/pages/receiver/receiver.tsx
@@ -5,22 +5,37 @@ export default function Receiver() {
   const [peer, setPeer] = useState<SimplePeer.Instance>()
   const [signals, setSignals] = useState<object[]>([])
   const [offer, setOffer] = useState("")
+  const [messages, setMessages] = useState<string[]>([])
+  const [composedMessage, setComposedMessage] = useState("")
 
   useEffect(() => {
     if (!peer) {
       const newPeer = new SimplePeer({ trickle: false, initiator: false })
+
       newPeer.on("signal", (e: SimplePeer.SignalData) => {
         console.log("## SIGNAL", { e })
         setSignals((prev) => [...prev, e])
       })
+
       newPeer.on("connect", () => {
-        console.log("## CONNECTED")
+        newPeer.send("Connected")
       })
+
       newPeer.on("data", (data: string) => {
         console.log(typeof data)
         // got a data channel message
         console.log("got a message from peer1: " + data)
       })
+
+      newPeer.on("data", (data: Uint8Array) => {
+        // Convert the Uint8Array to a Buffer
+        const buffer = Buffer.from(data)
+        // Convert the Buffer to text using toString() method
+        const text = buffer.toString("utf-8")
+        console.log({ data })
+        setMessages((prev) => [...prev, text])
+      })
+
       setPeer(newPeer)
     }
   }, [signals, peer])
@@ -28,31 +43,62 @@ export default function Receiver() {
   return (
     <div>
       <h1>Receiver Page</h1>
-      <div>
-        <input
-          type="text"
-          name="offer"
-          id="offer"
-          onChange={(e) => {
-            setOffer(e.target.value)
-          }}
-        />
-        <button
-          onClick={() => {
-            peer?.signal(offer)
-          }}
-        >
-          Accept Offer
-        </button>
-      </div>
-      <div>
-        <h2>Signals</h2>
-        <ul>
-          {signals.map((item) => (
-            <li key={JSON.stringify(item)}>{JSON.stringify(item, null, 2)}</li>
-          ))}
-        </ul>
-      </div>
+      {peer?.connected ? (
+        <div>
+          <h2>Messsages</h2>
+          <ul>
+            {messages.map((msg) => (
+              <li key={msg}>{msg}</li>
+            ))}
+          </ul>
+          <div>
+            <input
+              type="text"
+              name="message"
+              id="message"
+              onChange={(e) => {
+                setComposedMessage(e.target.value)
+              }}
+            />
+            <button
+              onClick={() => {
+                peer.send(composedMessage)
+                setComposedMessage("")
+              }}
+            >
+              Send Answer
+            </button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div>
+            <input
+              type="text"
+              name="offer"
+              id="offer"
+              onChange={(e) => {
+                setOffer(e.target.value)
+              }}
+            />
+            <button
+              onClick={() => {
+                peer?.signal(offer)
+              }}
+            >
+              Accept Offer
+            </button>
+          </div>
+          <div>
+            <h2>Signals</h2>
+            <ul>
+              {signals.map((item) => (
+                <li key={JSON.stringify(item)}>{JSON.stringify(item, null, 2)}</li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   )
 }
